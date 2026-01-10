5170ac941849074ba9b654be5e887800b34951a4
LotifurShabbir <lotifurshabbir54@gmail.com>
2026-01-10 22:38:48 +0600

    daily done

---
+++ Codeforces 2025/B - Yet Another MEX Problem.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX 1000006
+#define nline "\n"
+#define ll long long int
+#define pb push_back
+#define dbg(args...) do { cerr << #args << " : "; halum(args); } while (0)
+
+void halum() { cerr << endl; }
+
+template <typename T, typename... Rest>
+void halum(T arg, const Rest&... rest)
+{
+    cerr << arg << ' ';
+    halum(rest...);
+}
+
+ll gcd(ll a, ll b)
+{
+    return std::gcd(a, b);
+}
+
+ll lcm(ll a, ll b)
+{
+    return a / gcd(a, b) * b;
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    // For getting input from input.txt file
+    freopen("Input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("Output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--){
+        ll n, m;
+    cin >> n >> m;
+
+    vector<ll> a(n + 1);
+    vector<ll> b(n + 6, 0);
+
+    for (ll i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        a[i] = min(a[i], n + 1);
+    }
+
+    for (ll i = 1; i <= n; i++)
+        b[a[i]]++;
+
+    ll mex = 0;
+    while (b[mex])
+        mex++;
+
+    cout << min(mex, m - 1) << nline;
+    }
+
+    return 0;
+}
