fe737a308d7ceaffd14aafb2174301d96b0afc98
LotifurShabbir <lotifurshabbir54@gmail.com>
2026-01-13 21:07:31 +0600

    done

---
+++ Codeforces 2025/A - Anti-knapsack.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX 1000006
+#define nline "\n"
+#define ll long long int
+#define pb push_back
+#define dbg(args...) do { cerr << #args << " : "; halum(args); } while (0)
+
+void halum() { cerr << endl; }
+
+template <typename T, typename... Rest>
+void halum(T arg, const Rest&... rest)
+{
+    cerr << arg << ' ';
+    halum(rest...);
+}
+
+ll gcd(ll a, ll b)
+{
+    return std::gcd(a, b);
+}
+
+ll lcm(ll a, ll b)
+{
+    return a / gcd(a, b) * b;
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    // For getting input from input.txt file
+    freopen("Input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("Output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+
+        int x = k >> 1;
+        cout << x + n - k << nline;
+
+        for (int i = 1; i <= x; i++)
+            cout << k - i << " ";
+
+        for (int i = k + 1; i <= n; i++)
+            cout << i << " ";
+
+        cout << nline;
+    }
+
+    return 0;
+}
