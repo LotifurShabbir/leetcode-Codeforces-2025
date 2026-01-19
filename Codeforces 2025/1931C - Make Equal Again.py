3234da8570248c8bc8b1804c16fede6320309506
LotifurShabbir <lotifurshabbir54@gmail.com>
2026-01-19 20:42:13 +0600

    daily done

---
+++ Codeforces 2025/1931C - Make Equal Again.py
@@ -0,0 +1,24 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    i1 = 0
+    i2 = 0
+
+    while i1 < n and a[i1] == a[0]:
+        i1 += 1
+
+    while i2 < n and a[n - i2 - 1] == a[n - 1]:
+        i2 += 1
+
+    res = n
+
+    if a[0] == a[n - 1]:
+        res -= i1
+        res -= i2
+    else:
+        res -= max(i1, i2)
+
+    print(max(0, res))
