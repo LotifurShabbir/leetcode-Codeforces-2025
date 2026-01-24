71f7c2082a06c710fd2b2cabbc3327254ae06b26
LotifurShabbir <lotifurshabbir54@gmail.com>
2026-01-24 22:55:47 +0600

    daily codeforces done

---
+++ Codeforces 2025/D. Challenging Valleys.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX 1000006
+#define nline "\n"
+#define ll long long int
+#define pb push_back
+#define dbg(args...) do { cerr << #args << " : "; halum(args); } while(0)
+
+void halum() { cerr << endl; }
+
+template <typename T, typename... Rest>
+void halum(T arg, const Rest&... rest)
+{
+    cerr << arg << ' ';
+    halum(rest...);
+}
+
+ll gcd(ll a, ll b) { return std::gcd(a, b); } // C++17 std::gcd
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    // For getting input from input.txt file
+    freopen("Input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("Output.txt", "w", stdout);
+    #endif
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<ll> a(n), b;
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0 || a[i] != a[i - 1])
+                b.pb(a[i]);
+        }
+
+        int k = b.size();
+        int cnt = 0;
+
+        for (int i = 0; i < k; i++)
+        {
+            if ((i == 0 || b[i - 1] > b[i]) &&
+                (i == k - 1 || b[i] < b[i + 1])) cnt++;
+        }
+
+        if (cnt == 1) cout << "YES" << nline;
+        else cout << "NO" << nline;
+    }
+
+    return 0;
+}
